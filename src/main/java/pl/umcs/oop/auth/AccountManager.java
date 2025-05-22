21014d705160ea178e247d589bdf1c32e45be4d4
dariakoropenko <koropenko.daria@gmail.com>
2025-05-22 16:50:35 +0200

    Hashed passwords, updated authentication and getAccount methods

--- src/main/java/pl/umcs/oop/auth/AccountManager.java
+++ src/main/java/pl/umcs/oop/auth/AccountManager.java
@@ -1,8 +1,10 @@
 package pl.umcs.oop.auth;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import pl.umcs.oop.database.DatabaseConnection;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -31,23 +33,53 @@ public class AccountManager {
     }
 
     public void register(String username, String password) throws SQLException {
+        String hashedPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
         PreparedStatement stmt = db.getConnection().prepareStatement("""
                 INSERT INTO users (username, password) VALUES (?, ?);
                 """);
         stmt.setString(1, username);
-        stmt.setString(2, password);
+        stmt.setString(2, hashedPassword);
         stmt.executeUpdate();
     }
 
-/*    public boolean authenticate(String username, String password) {
-        return true;
+    public boolean authenticate(String username, String password) throws SQLException {
+        PreparedStatement stmt = db.getConnection().prepareStatement("""
+                SELECT * FROM users WHERE username = ? AND password = ?;
+                """);
+        stmt.setString(1, username);
+        ResultSet rs = stmt.executeQuery();
+        if(!rs.next()) {
+            return false;
+        }
+        String hashedPassword = rs.getString("password");
+        return BCrypt.verifyer().verify(password.toCharArray(), hashedPassword.toCharArray()).verified;
     }
 
-    public Account getAccount(int id) {
-        return new Account(12453, );
+    public Account getAccount(int id) throws SQLException {
+        PreparedStatement stmt = db.getConnection().prepareStatement("""
+                SELECT * FROM users WHERE id = ?;
+                """);
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+        if(rs.next()) {
+            int i = rs.getInt("id");
+            String username = rs.getString("username");
+        }
+        return null;
     }
 
-    public Account getAccount(String username) {
-        return new Account("name", 1235);
-    }*/
+    public Account getAccount(String username) throws SQLException {
+        PreparedStatement stmt = db.getConnection().prepareStatement("""
+                SELECT * FROM users WHERE id = ?;
+                """);
+        stmt.setString(1, username);
+        ResultSet rs = stmt.executeQuery();
+        if(rs.next()) {
+            int i = rs.getInt("id");
+            String u = rs.getString("username");
+
+            return new Account(i, u);
+        }
+        return null;
+    }
 }
